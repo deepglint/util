b76ab71e92f5329ecb6321866f15203116a38e5d
tongliu <tongliu@deepglint.com>
2013-12-19 17:45:54 +0800

    add something^_^

--- config/JsonConfig.go
+++ config/JsonConfig.go
@@ -3,10 +3,16 @@ package config
 import (
 	"encoding/json"
 	"os"
+	"strings"
 )
 
-func GetJsonConfig(config_path string, e interface{}) error {
+func GetJsonConfigFromFile(config_path string, e interface{}) error {
 	config_file, _ := os.Open(config_path)
 	decoder := json.NewDecoder(config_file)
+	return decoder.Decode(e)
+}
+
+func GetJsonConfigFromString(json_string string, e interface{}) error {
+	decoder := json.NewDecoder(strings.NewReader(json_string))
 	return decoder.Decode(e)
 }
