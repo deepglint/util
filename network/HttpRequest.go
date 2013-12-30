65423615b5b419c4476d49f35d36bb00ad7650e5
tongliu <tongliu@deepglint.com>
2013-12-30 11:10:29 +0800

    add http post

--- network/HttpRequest.go
+++ network/HttpRequest.go
@@ -3,10 +3,17 @@ package network
 import (
 	"github.com/liutong19890905/util/security"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func HttpGetSimpleRequest(url string) string {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println(err)
+		}
+	}()
 	client := &http.Client{}
 	reqest, _ := http.NewRequest("GET", url, nil)
 	response, _ := client.Do(reqest)
@@ -16,10 +23,29 @@ func HttpGetSimpleRequest(url string) string {
 }
 
 func HttpGetAuthRequest(url string, username string, password string) string {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println(err)
+		}
+	}()
 	client := &http.Client{}
 	reqest, _ := http.NewRequest("GET", url, nil)
 	auth_str := username + ":" + password
 	reqest.Header.Add("Authorization", "Basic "+security.EncodeBase64(auth_str))
+	response, _ := client.Do(reqest)
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	return string(body)
+}
+
+func HttpPostSimpleRequest(url string, data string) string {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println(err)
+		}
+	}()
+	client := &http.Client{}
+	reqest, _ := http.NewRequest("POST", url, strings.NewReader(data))
 	response, _ := client.Do(reqest)
 	defer response.Body.Close()
 	body, _ := ioutil.ReadAll(response.Body)
