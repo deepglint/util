b70b9b6eb8430360837127cee948a58f411fa1c6
tongliu <tongliu@deepglint.com>
2014-04-17 14:43:43 +0800

    update move & mkdir in github/deepglint/util/io/file

--- io/file/sysfile.go
+++ io/file/sysfile.go
@@ -3,6 +3,7 @@ package file
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -13,16 +14,16 @@ const (
 )
 
 type SysFile struct {
-	fType  int
-	fName  string
-	fLink  string
-	fSize  int64
-	fMtime time.Time
-	fPerm  os.FileMode
+	Type  int
+	Name  string
+	Link  string
+	Size  int64
+	Mtime time.Time
+	Perm  os.FileMode
 }
 
 type fs struct {
-	files []*SysFile
+	files []SysFile
 }
 
 func (self *fs) visit(path string, f os.FileInfo, err error) error {
@@ -37,24 +38,79 @@ func (self *fs) visit(path string, f os.FileInfo, err error) error {
 	} else {
 		tp = IsRegular
 	}
-	inoFile := &SysFile{
-		fName:  path,
-		fType:  tp,
-		fPerm:  f.Mode(),
-		fMtime: f.ModTime(),
-		fSize:  f.Size(),
+	inoFile := SysFile{
+		Name:  path,
+		Type:  tp,
+		Perm:  f.Mode(),
+		Mtime: f.ModTime(),
+		Size:  f.Size(),
 	}
 	self.files = append(self.files, inoFile)
 	return nil
 }
 
-func Visit(root string) (error, []*SysFile) {
-	self := fs{
-		files: make([]*SysFile, 0),
-	}
+func Visit(root string) (error, []SysFile) {
+	var files []SysFile
+	var self fs
+	self.files = files
 	err := filepath.Walk(root, func(path string, f os.FileInfo, err error) error {
 		return self.visit(path, f, err)
 	})
 
 	return err, self.files
+}
+
+func IsExist(name string) bool {
+	_, err := os.Stat(name)
+	return err == nil || os.IsExist(err)
+}
+
+func Remove(name string) error {
+	return os.Remove(name)
+}
+
+func Move(oldname string, newname string) error {
+	dirs := strings.Split(newname, "/")
+	dirname := ""
+	delta := 1
+
+	if len(dirs[len(dirs)-1]) == 0 {
+		delta = 2
+	}
+
+	for i := 0; i < len(dirs)-delta-1; i++ {
+		dirname += dirs[i] + "/"
+	}
+	dirname += dirs[len(dirs)-delta-1]
+
+	if !IsExist(dirname) {
+		Mkdir(dirname)
+	}
+
+	return os.Rename(oldname, newname)
+}
+
+func Mkdir(name string) error {
+	err := os.Mkdir(name, 0700)
+	if err != nil && strings.Contains(err.Error(), "no such file or directory") {
+		dirs := strings.Split(name, "/")
+		newname := ""
+		delta := 1
+
+		if len(dirs[len(dirs)-1]) == 0 {
+			delta = 2
+		}
+
+		for i := 0; i < len(dirs)-delta-1; i++ {
+			newname += dirs[i] + "/"
+		}
+		newname += dirs[len(dirs)-delta-1]
+		inner_err := Mkdir(newname)
+		if inner_err == nil {
+			err = os.Mkdir(name, 0700)
+		} else {
+			return inner_err
+		}
+	}
+	return err
 }
